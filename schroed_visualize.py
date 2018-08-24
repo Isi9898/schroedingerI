fbbc91f2bcd2c195afd89dea552f4e476c3f382e
Isi9898 <40384074+Isi9898@users.noreply.github.com>
2018-08-24 17:22:05 +0200

    Add files via upload
    improve the score

--- schroed_visualize.py
+++ schroed_visualize.py
@@ -10,60 +10,47 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 
-def load_data():
+def load_data(bulge_factor):
     """load data files"""
-    #directory = "/Users/isi/Desktop/schroedinger/harmonic"
-    x = np.loadtxt("potential.dat")[:, 0]
+    xx = np.loadtxt("potential.dat")[:, 0]
     potential = np.loadtxt("potential.dat")[:, 1]
     energies = np.loadtxt("energies.dat")
     wavefuncs = np.loadtxt("wavefuncs.dat")[:, 1:]
     expvalues = np.loadtxt("expvalues.dat")[:, 0]
     uncertainty = np.loadtxt("expvalues.dat")[:, 1]
 
-    return x, potential, energies, wavefuncs, expvalues, uncertainty
+    #plotting potential, energies, wafefunctions and expactationvalues
+    plt.subplot(1, 2, 1)
+    plt.plot(xx, potential, color="black")
 
+    aa = bulge_factor
 
-x, potential, energies, wavefuncs, expvalues, uncertainty = load_data()
+    bb = len(energies)
+    for ii in range(0, bb):
+        energy = np.linspace(energies[ii], energies[ii], len(xx))
+        plt.plot(xx, energy, color="gray")
+        plt.plot(expvalues, energies, "xg")
+        if ii % 2 == 0:
+            plt.plot(xx, aa*wavefuncs[:, ii]+energies[ii], color="blue")
+        else:
+            plt.plot(xx, aa*wavefuncs[:, ii]+energies[ii], color="red")
+    plt.xlim(xx.min() * 1.1, xx.max() * 1.1)
+    plt.ylim(energies.min() * (-1.1), energies.max() * 1.1)
+    plt.xlabel("$x$ [Bohr]")
+    plt.ylabel("Energy [Hartree]")
+    plt.title("Potential, eigenstates, ($x$)")
 
+    #plotting energies and uncertainty
+    plt.subplot(1, 2, 2)
+    bb = len(energies)
+    for ii in range(0, bb):
+        energy = np.linspace(energies[ii], energies[ii], len(xx))
+        plt.plot(xx, energy, color="gray")
+        plt.plot(uncertainty, energies, "+m")
+        plt.yticks([])
+    plt.xlim(0, uncertainty.max() * 1.1)
+    plt.ylim(energies.min() * (-1.1), energies.max() * 1.1)
+    plt.xlabel("[Bohr]")
+    plt.title("$\sigma_\mathrm{x}$")
 
-"""plotting potential, energies, wafefunctions and expactationvalues"""
-plt.subplot(1, 2, 1)
-plt.plot(x, potential, color="black")
-
-#a = bulge_factor
-a= 0.3
-N = len(energies)
-for ii in range(0, N):
-    energy = np.linspace(energies[ii], energies[ii], len(x))
-    plt.plot(x, energy, color="gray")
-    plt.plot(expvalues, energies, "xg")
-    if ii%2 == 0:
-        plt.plot(x, a*wavefuncs[:, ii]+energies[ii], color="blue")
-    else:
-        plt.plot(x, a*wavefuncs[:, ii]+energies[ii], color="red")
-plt.xlim(x.min() * 1.1, x.max()* 1.1)
-plt.ylim(energies.min() * (-1.1), energies.max() * 1.1)
-
-
-plt.xlabel("$x$ [Bohr]")
-plt.ylabel("Energy [Hartree]")
-plt.title("Potential, eigenstates, ($x$)")
-
-"""plotting engergies and uncertainty"""
-plt.subplot(1, 2, 2)
-N = len(energies)
-for ii in range(0, N):
-    energy = np.linspace(energies[ii], energies[ii], len(x))
-    plt.plot(x, energy, color="gray")
-    plt.plot(uncertainty, energies, "+m")
-    plt.yticks([])
-plt.xlim(0, uncertainty.max() * 1.1)
-plt.ylim(energies.min() * (-1.1), energies.max() * 1.1)
-
-
-
-plt.xlabel("[Bohr]")
-plt.title("$\sigma_\mathrm{x}$")
-
-plt.show()
-plt.savefig("solution_schoedinger.pdf", format="pdf")
+    plt.savefig("solution_schoedinger.pdf", format="pdf")
